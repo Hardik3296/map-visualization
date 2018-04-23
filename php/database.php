093895846ef3df64b8b34a7c8601a07f90b4c825
Hardik3296 <hardikgaur3296@gmail.com>
2018-04-24 00:02:49 +0530

    Php file establishing connection to database

---
+++ php/database.php
@@ -0,0 +1,7 @@
+<?php
+	$username = "root";
+	$password = "";
+	$dbname = "skylark_drones";
+	$servername = "localhost";
+	$connection = mysqli_connect($servername, $username, $password, $dbname);
+?>
