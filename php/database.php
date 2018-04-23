af4c0a23745eb2b1f5accbbd5264d9771b6e1f7d
Hardik3296 <hardikgaur3296@gmail.com>
2018-04-24 00:27:50 +0530

    Updated project

--- php/database.php
+++ php/database.php
@@ -4,4 +4,8 @@
 	$dbname = "skylark_drones";
 	$servername = "localhost";
 	$connection = mysqli_connect($servername, $username, $password, $dbname);
+	if(mysqli_connect_errno()) {
+		$return = array('error'=>'true');
+		echo $return;
+	}
 ?>
